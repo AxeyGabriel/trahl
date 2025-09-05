1fec001e335babd885eb69234d2129a7ae0c6a98
Axey Endres <axey.endres@gmail.com>
2025-09-04 22:00:23 -0300

    Add signal handling

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,24 @@ use crate::config::{SystemConfig};
 use crate::args::{parse_args};
 use crate::logs::{init_logging};
 
+use std::io::Error;
 use tracing::info;
+use signal_hook::flag;
+use signal_hook::consts::signal::*;
+use std::sync::{Arc, OnceLock};
+use std::sync::atomic::{AtomicBool, Ordering};
 
-fn main() -> Result<(), lexopt::Error> {
-    let args = parse_args()?;
+pub static S_TERMINATE: OnceLock<Arc<AtomicBool>> = OnceLock::new();
+
+fn main() -> Result<(), Error> {
+    let args = match parse_args() {
+        Ok(args) => args,
+        Err(e) => {
+            eprintln!("Failed to parse arguments: {}", e);
+            std::process::exit(1);
+        }
+    };
+
     let config = match SystemConfig::parse(&args.config_file) {
         Ok(cfg) => cfg,
         Err(e) => {
@@ -24,8 +38,24 @@ fn main() -> Result<(), lexopt::Error> {
     }
 
     let _guard = init_logging(config.log_file);
+
+    S_TERMINATE.set(Arc::new(AtomicBool::new(false))).unwrap();
+    let s_term = S_TERMINATE.get().unwrap();
+    flag::register(SIGINT, s_term.clone())?;
+    flag::register(SIGTERM, s_term.clone())?;
     
     info!("TRAHL is initializing...");
+    
+    loop {
+        let terminate = S_TERMINATE.get().unwrap();
+        if terminate.load(Ordering::Relaxed) {
+            info!("term");
+            break;
+        } else {
+            std::thread::sleep(std::time::Duration::from_secs(1));
+        }
+    }
+    
 
     Ok(())
 }
