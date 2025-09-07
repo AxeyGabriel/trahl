25120043ec0c52f4b1c5de1b9a8bbcb2158dfa79
Axey Endres <axey.endres@gmail.com>
2025-09-06 22:08:22 -0300

    Add lua test

--- experiments/lua/src/main.rs
+++ experiments/lua/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use mlua::{Lua, Result};
+use std::fs;
+
+fn main() -> Result<()> {
+    let lua = Lua::new();
+    let globals = lua.globals();
+
+    globals.set("myvar", "axey")?;
+
+    let script = fs::read_to_string("plugin.lua").expect("read file failed");
+    lua.load(&script).set_name("plugin.lua").exec()?;
+
+    Ok(())
 }
