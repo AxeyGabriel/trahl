cde170fffb06e7dfa74c0c19e8721dec9659eb47
Axey Endres <axey.endres@gmail.com>
2025-09-07 15:58:54 -0300

    Add raw body support for http ffi

--- src/lua/http.rs
+++ src/lua/http.rs
@@ -2,9 +2,13 @@ use mlua::{Error, Lua, Result, Table};
 use reqwest::header::{HeaderMap, HeaderName, HeaderValue};
 
 pub async fn _http_request(_: Lua,
-    (method_str, url, headers_t): (String, String, Option<Table>))
--> Result<(u16, String)>
-{
+    (
+        method_str,
+        url,
+        headers_t,
+        raw_body
+    ): (String, String, Option<Table>, Option<String>)
+) -> Result<(u16, String)> {
     let method = method_str
         .parse::<reqwest::Method>()
         .map_err(Error::external)?;
@@ -20,9 +24,17 @@ pub async fn _http_request(_: Lua,
     }
 
     let client = reqwest::Client::new();
-    let res = client
+    let builder = client
         .request(method, &url)
-        .headers(headers)
+        .headers(headers);
+
+    let builder = if let Some(body) = raw_body {
+        builder.body(body)
+    } else {
+        builder
+    };
+
+    let res = builder
         .send()
         .await
         .map_err(Error::external)?;
@@ -53,7 +65,7 @@ mod tests {
 
         let lua_code = format!(r#"
             function test_request()
-                local status, body = http_request("GET", "{}/get", nil)
+                local status, body = http_request("GET", "{}/get")
                 return status, body
             end
         "#, HTTPBIN);
@@ -94,6 +106,39 @@ mod tests {
 
         assert_eq!(status, 200);
         assert!(body.contains(r#"X-Test-Header": "abc"#));
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_http_request_body() -> Result<()> {
+        init_tracing();
+        let lua = Lua::new();
+        let http_ffi = lua.create_async_function(_http_request)?;
+        lua.globals().set("http_request", http_ffi)?;
+
+        let lua_code = format!(r#"
+            function test_request()
+                local headers = {{
+                    ["Content-Type"] = "application/x-www-form-urlencoded",
+                    ["accept"] = "application/json",
+                }}
+                local raw_body = "param1=123&param2=abc"
+                local status, body = http_request("POST", "{}/post", headers, raw_body)
+                return status, body
+            end
+        "#, HTTPBIN);
+
+        lua.load(lua_code).exec_async().await?;
+
+        let test_fn: Function = lua.globals().get("test_request")?;
+        let (status, body): (u16, String) = test_fn.call_async(()).await?;
+        let json: serde_json::Value = serde_json::from_str(&body).unwrap();
+        let form_data = &json["form"];
+
+        assert_eq!(status, 200);
+        assert_eq!(form_data["param1"], "123");
+        assert_eq!(form_data["param2"], "abc");
 
         Ok(())
     }
